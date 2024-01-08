13b167523e0ff7e8a7850ef1e396f2cd56ff7dae
alisamettan <alisamet_tan@hotmail.com>
2024-01-08 17:49:34 +0300

    Success Page'i yazıldı

--- src/SuccessPage.jsx
+++ src/SuccessPage.jsx
@@ -1,4 +1,15 @@
-export default function SuccessPage(){
-    return(<>
+import logo from './assets/logo.svg'
+
+export default function SuccessPage() {
+    return (<>
+        <div className="successpage">
+            <div className='main'>
+                <img className='successImg' src={logo} alt="" />
+                <div>
+                    <p>TEBRIKLER!</p>
+                    <p>SİPARİŞİNİZ ALINDI!</p>
+                </div>
+            </div>
+        </div>
     </>)
 }
