e0cd206bd9713d204a2489fe95ab564622a59baa
alisamettan <alisamet_tan@hotmail.com>
2024-01-09 09:42:28 +0300

    checkboxlara 10 tane işaretleyince geri kalanlarını disabled etme özelliği getirildi

--- src/FormAlani.jsx
+++ src/FormAlani.jsx
@@ -4,26 +4,30 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import './FormAlani.css'
 
 export default function FormAlani(props) {
-    const {toplam,setToplam,ek,setEk}=props
-    
-    const history=useHistory();
+    const { toplam, setToplam, ek, setEk } = props
+
+    const history = useHistory();
 
     const [form, setForm] = useState({});
     //const [ek, setEk] = useState(0);
-    const [adet,setAdet]=useState(1);
+    const [adet, setAdet] = useState(1);
     //const [toplam,setToplam]=useState(0)
-    const [errors,setErrors]=useState({})
-    const [isValid,setIsValid]=useState(true)
-    const [detay,setDetay]=useState([])
+    const [errors, setErrors] = useState({})
+    const [isValid, setIsValid] = useState(true)
+    const [selectedCheckboxes, setSelectedCheckboxes] = useState(0);
 
-    useEffect(()=>{
-        if(form.boyut && form.kalinlik){
+    useEffect(() => {
+        if (form.boyut && form.kalinlik) {
             setIsValid(true)
         }
-        else{
+        else {
             setIsValid(false)
         }
-    },[form])
+        const checkedCheckboxes = document.querySelectorAll('.checkbox:checked').length;
+        setSelectedCheckboxes(checkedCheckboxes);
+        
+    }, [form])
+    const disableCheckboxes = selectedCheckboxes >= 10;
 
     const handleChange = (event) => {
         let { value, name, type, checked } = event.target;
@@ -31,35 +35,38 @@ export default function FormAlani(props) {
         setForm({ ...form, [name]: value })
 
         if (name == 'pepperoni' || name == 'sosis' || name == 'jambon' || name == 'izgara' || name == 'sogan' || name == 'domates' || name == 'misir'
-            || name == 'sucuk' ||  name == 'biber' ||  name == 'ananas' || name == 'kabak'|| name=='jalepeno') {
+            || name == 'sucuk' || name == 'biber' || name == 'ananas' || name == 'kabak' || name == 'jalepeno') {
             if (value === true) {
-                setEk(ek + 5)               
+                setEk(ek + 5)
             } else {
                 setEk(ek - 5)
             }
         }
-        if(name=='boyut'){
-            if(value===true){     
-                setErrors({...errors,[name]:false})
-            }else{
-                setErrors({...errors,[name]:true})
+
+
+
+        if (name == 'boyut') {
+            if (value === true) {
+                setErrors({ ...errors, [name]: false })
+            } else {
+                setErrors({ ...errors, [name]: true })
             }
         }
-        if(name=='kalinlik'){
-            if(value===true){     
-                setErrors({...errors,[name]:false})
-            }else{
-                setErrors({...errors,[name]:true})
+        if (name == 'kalinlik') {
+            if (value === true) {
+                setErrors({ ...errors, [name]: false })
+            } else {
+                setErrors({ ...errors, [name]: true })
             }
         }
 
     }
 
-    function handleSubmit(event){
+    function handleSubmit(event) {
         event.preventDefault();
 
-        axios.post('https://reqres.in/api/users',form).then(response=>{
-            const data=response.data
+        axios.post('https://reqres.in/api/users', form).then(response => {
+            const data = response.data
             console.log(data)
             history.push('/success')
         })
@@ -68,14 +75,14 @@ export default function FormAlani(props) {
     function handleClickSum(event) {
         event.preventDefault()
         setToplam(toplam + 85.50)
-        setAdet(adet+1)
+        setAdet(adet + 1)
     }
     function handleClickMin(event) {
         event.preventDefault()
         if (toplam > 1) {
             setToplam(toplam - 85.50)
-            setAdet(adet-1)
-        }else{
+            setAdet(adet - 1)
+        } else {
             setToplam(toplam)
         }
     }
@@ -85,13 +92,13 @@ export default function FormAlani(props) {
             <div>
                 <p style={{ fontWeight: 'bold', fontSize: '18px' }}>Boyut Seç <span>*</span></p>
                 <div className="boyut">
-                    <label ><input onChange={handleChange} type="radio" id="'kücük" name="boyut" value='kücük' checked={form.boyut=='kücük'?true:false} />
+                    <label ><input onChange={handleChange} type="radio" id="'kücük" name="boyut" value='kücük' checked={form.boyut == 'kücük' ? true : false} />
                         Küçük</label>
-                    <label><input onChange={handleChange} type="radio" id="'orta" name="boyut" value='orta' checked={form.boyut=='orta'?true:false} />
+                    <label><input onChange={handleChange} type="radio" id="'orta" name="boyut" value='orta' checked={form.boyut == 'orta' ? true : false} />
                         Orta</label>
-                    <label><input onChange={handleChange} type="radio" id="'büyük" name="boyut" value='büyük' checked={form.boyut=='büyük'?true:false} />
+                    <label><input onChange={handleChange} type="radio" id="'büyük" name="boyut" value='büyük' checked={form.boyut == 'büyük' ? true : false} />
                         Büyük</label>
-                        {!errors.boyut && <p style={{color:'red',marginTop:'0',fontSize:'12px'}}>Lütfen bir boyut seçiniz!</p>}
+                    {!errors.boyut && <p style={{ color: 'red', marginTop: '0', fontSize: '12px' }}>Lütfen bir boyut seçiniz!</p>}
                 </div>
             </div>
             <div>
@@ -102,7 +109,7 @@ export default function FormAlani(props) {
                     <option value="orta">Orta</option>
                     <option value="kalin">Kalın</option>
                 </select>
-                {!errors.kalinlik && <p style={{color:'red',marginTop:'0',fontSize:'12px'}}>Lütfen bir kalınlık seçiniz!</p>}
+                {!errors.kalinlik && <p style={{ color: 'red', marginTop: '0', fontSize: '12px' }}>Lütfen bir kalınlık seçiniz!</p>}
             </div>
         </div>
         <div style={{ marginTop: '2rem' }}>
@@ -111,39 +118,39 @@ export default function FormAlani(props) {
             <div className="ekmalzemeler">
                 <div >
                     <div className="birinci" >
-                        <label><input onChange={handleChange} checked={form.pepperoni} type="checkbox" name="pepperoni" id="pepperoni" />
+                        <label><input className="checkbox" onChange={handleChange} disabled={disableCheckboxes} checked={form.pepperoni} type="checkbox" name="pepperoni" id="pepperoni" />
                             Pepperoni</label>
-                        <label> <input onChange={handleChange} checked={form.sosis}  type="checkbox" name="sosis" id="sosis" />
+                        <label> <input onChange={handleChange} className="checkbox" disabled={disableCheckboxes} checked={form.sosis} type="checkbox" name="sosis" id="sosis" />
                             Sosis</label>
-                        <label><input onChange={handleChange} checked={form.jambon} type="checkbox" name="jambon" id="jambon" />
+                        <label><input onChange={handleChange} className="checkbox" disabled={disableCheckboxes} checked={form.jambon} type="checkbox" name="jambon" id="jambon" />
                             Kanada Jambonu</label>
-                        <label><input onChange={handleChange} checked={form.izgara} type="checkbox" name="izgara" id="izgara" />
+                        <label><input onChange={handleChange} className="checkbox" disabled={disableCheckboxes} checked={form.izgara} type="checkbox" name="izgara" id="izgara" />
                             Tavuk Izgara</label>
-                        <label><input onChange={handleChange} checked={form.sogan} type="checkbox" name="sogan" id="sogan" />
+                        <label><input onChange={handleChange} className="checkbox" disabled={disableCheckboxes} checked={form.sogan} type="checkbox" name="sogan" id="sogan" />
                             Soğan</label>
                     </div>
                 </div>
                 <div>
                     <div className="birinci" >
-                        <label><input onChange={handleChange} checked={form.domates} type="checkbox" name="domates" id="domates" />
+                        <label><input onChange={handleChange} className="checkbox" disabled={disableCheckboxes} checked={form.domates} type="checkbox" name="domates" id="domates" />
                             Domates</label>
-                        <label> <input onChange={handleChange} checked={form.misir} type="checkbox" name="misir" id="misir" />
+                        <label> <input onChange={handleChange} className="checkbox" disabled={disableCheckboxes} checked={form.misir} type="checkbox" name="misir" id="misir" />
                             Mısır</label>
-                        <label><input onChange={handleChange} checked={form.sucuk} type="checkbox" name="sucuk" id="sucuk" />
+                        <label><input onChange={handleChange} className="checkbox" disabled={disableCheckboxes} checked={form.sucuk} type="checkbox" name="sucuk" id="sucuk" />
                             Sucuk</label>
-                        <label><input onChange={handleChange} checked={form.jalepeno} type="checkbox" name="jalepeno" id="jalepeno" />
+                        <label><input onChange={handleChange} className="checkbox" disabled={disableCheckboxes} checked={form.jalepeno} type="checkbox" name="jalepeno" id="jalepeno" />
                             Jalepeno</label>
-                            <label><input onChange={handleChange} checked={form.biber} type="checkbox" name="biber" id="biber" />
+                        <label><input onChange={handleChange} className="checkbox" disabled={disableCheckboxes} checked={form.biber} type="checkbox" name="biber" id="biber" />
                             Biber</label>
-                       
+
                     </div>
                 </div>
                 <div>
                     <div className="birinci" >
-                       
-                        <label><input onChange={handleChange} checked={form.ananas} type="checkbox" name="ananas" id="ananas" />
+
+                        <label><input onChange={handleChange} className="checkbox" disabled={disableCheckboxes} checked={form.ananas} type="checkbox" name="ananas" id="ananas" />
                             Ananas</label>
-                        <label><input onChange={handleChange} checked={form.kabak} type="checkbox" name="kabak" id="kabak" />
+                        <label><input onChange={handleChange} className="checkbox" disabled={disableCheckboxes} checked={form.kabak} type="checkbox" name="kabak" id="kabak" />
                             Kabak</label>
                     </div>
                 </div>
@@ -166,7 +173,7 @@ export default function FormAlani(props) {
                     </div>
                     <div className="siparisToplam" style={{ color: 'red' }}>
                         <p>Toplam</p>
-                        <p>{85.50 + ek+toplam}₺</p>
+                        <p>{85.50 + ek + toplam}₺</p>
                     </div>
                 </div>
                 <button type="submit" disabled={!isValid} style={{ marginTop: '0px', borderRadius: '0px' }} className="siparisver">Sipariş ver</button>
